541969d2b38eee3d7e517d73e3a2b822680d346e
Jose Fenic Peiteado Padilla <alu0101227023@ull.edu.es>
2023-10-09 07:11:27 +0100

    cifrado

--- medio/semana1/cifrado/main.cc
+++ medio/semana1/cifrado/main.cc
@@ -1,21 +1,50 @@
 #include<bits/stdc++.h>
 
 
-std::string fase1(std::string &aux){
+std::string faseuno(std::string aux){
     std::string result = aux;
+    int j = aux.size()-1;
+    int x = 0;
     for(int i = 0; i < aux.size();i++){
-        result[i] = aux[i];
+        result[x] = aux[i];
+        x++;
+        i++;
+        if(i < aux.size()){
+        result[j] = aux[i];
+        j--;
+        }else{
+            break;
+        }
     }
+    std::cout << result << "\n";
+    return result;
 }
 
-std::string fase2(std::string &aux){
-
+std::string fasedos(std::string aux){
+    for(int i = 0; i <aux.size();i++){
+        int init = i;
+        int fin = i;
+        std::string inv;
+        while((i<aux.size()) && ((aux[i] != 'a' && aux[i] != 'e' && aux[i] != 'i'&& aux[i] != 'o' && aux[i] != 'u'&& aux[i] != 'A' && aux[i] != 'E' && aux[i] != 'I' && aux[i] != 'O' && aux[i] != 'U')) ){
+            fin++;
+            inv+=aux[i];  
+            i++;
+       }
+        if(std::abs(fin-init) >= 2){
+            for(int j = init;j<fin;j++){
+                aux[j] = inv[(fin-init)-1-(j-init)];
+                            }            
+            }
+    }
+    
+    return aux;
 }
 
 int main(){
     std::string aux;
     while(std::getline(std::cin,aux)){
-        fase1(aux);
+       std::string prueba = faseuno(aux);
+       std::cout << fasedos(prueba) << "\n";
     }
-    return;
+    return 0;
 }
